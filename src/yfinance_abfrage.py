d37e4238515ab3b8d60f533dfb8c3dcbfff715cb
merinobro <116284900+merinobro@users.noreply.github.com>
2026-03-24 12:07:25 +0000

    fix: requirements.txt ergänzt, fix: python-app.yml pipeline Verbesserungen, add: test_app.py Dummy Test damit die pipeline läuft,  add: yfinance_abfrage.py Versuch Daten über yfinance library zu ziehen

---
+++ src/yfinance_abfrage.py
@@ -0,0 +1,19 @@
+# Abfrage über yfinance library
+# Vorteil 1: müssen keinen API key managen, was später zu Nervkram mit der build pipeline führen kann
+# Vorteil 2: wir bekommen direkt "dataframes", die sich einfach mit pandas verarbeiten lassen, statt mühsam Daten via json zu parsen
+import yfinance as yf
+
+def get_oil_price():
+    # Holt die Daten für Brent Oil
+    oil = yf.Ticker("BZ=F")
+    # Holt den neuesten Kurs (1 Tag Historie)
+    data = oil.history(period="1d")
+    # bis einschließlich des gestrigen closing Kurses (-1)
+    return data['Close'].iloc[-1]
+
+if __name__ == "__main__":
+    try:
+        price = get_oil_price()
+        print(f"Aktueller Brent Öl Preis: {price:.2f} USD")
+    except Exception as e:
+        print(f"Fehler beim Abruf: {e}")
