946dc6b3c74b2ec6735ed71a7bf2c94dd2af8320
luca <lucadora@gmx.de>
2026-03-25 08:09:48 +0100

    all apis impl

--- src/yfinance_abfrage.py
+++ src/yfinance_abfrage.py
@@ -7,18 +7,32 @@ from apis import yfinance_api as yf_api
 from apis.finance_api import FinanceApi
 
 BRENT_OIL = "BZ=F"
+NATURAL_GAS = "NG=F"
+EUR_USD = "EURUSD=X"
 api: FinanceApi = yf_api.YfinanceApi()
 
 def get_oil_price():
-    # Holt die Daten für Brent Oil
     oil = api.get_data(BRENT_OIL)
+    return __extract_last_from_ticker(oil)
 
+def get_gas_price():
+    gas = api.get_data(NATURAL_GAS)
+    return __extract_last_from_ticker(gas)
+
+def get_eur_usd():
+    eur = api.get_data(EUR_USD)
+    return __extract_last_from_ticker(eur)
+
+def __check_is_ticker__(result):
     # TODO noch zu klären ob es einen Zweck gibt die andere Impl zu nutzen
-    if not isinstance(oil, yfinance.Ticker):
+    if not isinstance(result, yfinance.Ticker):
         raise TypeError("Momentan nur für yFinance implementiert")
 
+def __extract_last_from_ticker(result):
+    __check_is_ticker__(result)
+
     # Holt den neuesten Kurs (1 Tag Historie)
-    data = oil.history(period="1d")
+    data = result.history(period="1d")
     # bis einschließlich des gestrigen closing Kurses (-1)
     return data['Close'].iloc[-1]
 
