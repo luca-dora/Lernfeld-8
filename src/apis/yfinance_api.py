3ec25082fc6aa0506150cb0751288b69e0bb4652
luca <lucadora@gmx.de>
2026-03-31 12:51:08 +0200

    deleted finance api superclass

--- src/apis/yfinance_api.py
+++ src/apis/yfinance_api.py
@@ -1,15 +1,14 @@
 import yfinance as yf
 from pandas import Series
-from typing_extensions import override
-
-from .finance_api import FinanceApi
 
 
-class YfinanceApi(FinanceApi):
+class YfinanceApi:
+    def __init__(self, period: str = "5d", interval: str = "5m"):
+        self.period = period
+        self.interval = interval
 
-    @override
-    def get_data(self, tickers: list[str], period: str = "5d", interval: str = "5m") -> Series:
-        data = yf.download(tickers, period=period, interval=interval)
+    def get_data(self, tickers: list[str]) -> Series:
+        data = yf.download(tickers, period=self.period, interval=self.interval)
 
         # nur die Spalte 'Close' benötigt
         return data['Close']
--- src/yfinance_abfrage.py
+++ src/yfinance_abfrage.py
@@ -1,29 +1,26 @@
-# Abfrage über yfinance library
-# Vorteil 1: müssen keinen API key managen, was später zu Nervkram mit der build pipeline führen kann
-# Vorteil 2: wir bekommen direkt "dataframes", die sich einfach mit pandas verarbeiten lassen, statt mühsam Daten via json zu parsen
 from pandas import Series
 
 from apis import yfinance_api as yf_api
-from apis.finance_api import FinanceApi
+from apis.yfinance_api import YfinanceApi
 
 BRENT_OIL = "BZ=F"
 NATURAL_GAS = "NG=F"
 EUR_USD = "USDEUR=X"
-api: FinanceApi = yf_api.YfinanceApi()
+api: YfinanceApi = yf_api.YfinanceApi()
 
 
 def get_latest_oil_price() -> float:
-    oil = api.get_data(BRENT_OIL)
+    oil = api.get_data([BRENT_OIL])
     return __extract_latest_from_ticker(oil)
 
 
 def get_latest_gas_price() -> float:
-    gas = api.get_data(NATURAL_GAS)
+    gas = api.get_data([NATURAL_GAS])
     return __extract_latest_from_ticker(gas)
 
 
 def get_usd_eur() -> float:
-    eur = api.get_data(EUR_USD)
+    eur = api.get_data([EUR_USD])
     return __extract_latest_from_ticker(eur)
 
 
